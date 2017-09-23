ae58576d410794aae435f190d9fb1f182f9279bc
Humzah <humzahmerchant@gmail.com>
2017-09-23 10:11:12 -0700

    End of NavMesh Obstacles

---
+++ Assets/blockScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var rand = Random.Range(-0.3f, 0.3f);
+		this.transform.position = new Vector3(this.transform.position.x,this.transform.position.y, this.transform.position.z + rand);
+	}
+}
